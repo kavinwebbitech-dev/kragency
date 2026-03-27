85da9b7ffccf0854140e451fc6e03da9a5f38eef
webbitech <vigneshwebbitech@gmail.com>
2026-03-27 12:33:27 +0530

    result bug fixing amout mismatch

--- resources/views/admin/customer_orders/index.blade.php
+++ resources/views/admin/customer_orders/index.blade.php
@@ -146,7 +146,7 @@
                                     if (provider) params.push('provider=' + encodeURIComponent(provider));
                                     if (time) params.push('time=' + encodeURIComponent(time));
                                     if (params.length) url += '?' + params.join('&');
-                                    window.open(url);
+                                    window.open(url, '_blank');
                                 });
                             });
                         </script>
